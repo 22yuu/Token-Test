81f98c179fa65db1d6dcc6a9d50252a5774938ca
yuu <22yuu@naver.com>
2021-08-15 23:38:32 +0900

    commit test

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static string img_path = Application.StartupPath +  @"\resource\testimg\";
+        string[,] data;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] image_files = GetSearchFile(img_path);
+
+            foreach (string fname in image_files)
+            {
+                OCRProcess(fname);
+            }
+        }
+
+        async Task OCRProcess(string fileName)
+        {
+            Language language = new Language("ko");
+            FileStream stream = File.OpenRead(fileName);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+            SoftwareBitmap bitmap = await decoder.GetSoftwareBitmapAsync();
+            OcrEngine engine = OcrEngine.TryCreateFromLanguage(language);
+            OcrResult result = await engine.RecognizeAsync(bitmap).AsTask();
+
+            
+            string FileNameOnly = Path.GetFileNameWithoutExtension(fileName);
+            //string category = Regex.Replace(FileNameOnly, @"\d","");
+            string save_text_Ppath = Application.StartupPath + @"\resource\testxt\" + FileNameOnly +".txt";
+            Console.WriteLine(save_text_Ppath);
+            string[] text = { result.Text };
+            File.WriteAllLines(save_text_Ppath, text);
+        }
+
+        public string[] GetSearchFile(string _strPath)
+        {
+            string[] files = { "", };
+            try
+            {
+                files = Directory.GetFiles(_strPath, "*.*", SearchOption.AllDirectories);
+
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return files;
+        }
+    }
+}
---
+++ testocr2/testocr2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using System.IO;
+
+namespace testocr2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async Task OCRProcess()
+        {
+            Language language = new Language("ko");
+
+            FileStream stream = File.OpenRead(@"test2.jpg");
+
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+            SoftwareBitmap bitmap = await decoder.GetSoftwareBitmapAsync();
+
+            OcrEngine engine = OcrEngine.TryCreateFromLanguage(language);
+            OcrResult result = await engine.RecognizeAsync(bitmap).AsTask();
+
+            Clipboard.SetText(result.Text);
+        }
+
+        private void OCRProcess(object sender, EventArgs e)
+        {
+            OCRProcess();
+        }
+    }
+}
